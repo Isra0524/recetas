51dcb7780263dbd7ef085c74322f603c4cdec1e3
Fernanda2419 <fernanda.moreno@espoch.edu.ec>
2023-07-07 02:13:06 -0500

    Cambios Fernanda

---
+++ client/src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  url: 'http://localhost:4200/',
+  urlLogOut: 'http://localhost:4200/',
+  // urlApi:
+  // 'https://api-recetas.rubenvn.com/', //server Railway
+  urlApi: 'http://localhost:4400/',
+};
